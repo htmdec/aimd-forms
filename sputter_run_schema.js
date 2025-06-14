e5bfc3a1764563311c6476a6203b2eb0ed3ba2a6
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2025-06-14 09:12:38 -0500

    Address final review remarks

--- sputter_run_schema.js
+++ sputter_run_schema.js
@@ -1,1 +1,10 @@
 Handlebars.registerHelper('multiply', function (a, b) { return a * b; });
+Handlebars.registerHelper('divide', function (a, b) { 
+  if (a === undefined || a === null ) {
+    return -1;
+  }
+  if (b === undefined || b === null || b === 0) {
+    return -1;
+  }
+  return a / b;
+});
